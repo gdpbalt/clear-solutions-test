00d15bba17166eeffbf3ef119fc83ba776bfdba8
gdp <gdp@zigzag.ints.net>
2022-09-12 16:38:39 +0300

    initial

---
+++ src/main/java/com/example/clearsolutionstest/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.example.clearsolutionstest.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    @GetMapping
+    public String getAll() {
+        return "Hello mate";
+    }
+}
